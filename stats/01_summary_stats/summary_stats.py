27a1cca2c76d42c3b10f5c8f27152535bb15a863
Tovio Roberts <clownfragment@gmail.com>
2020-12-08 19:39:28 -0700

    Auto Updated: Tue 39:19 Dec 08 2020

--- stats/01_summary_stats/summary_stats.py
+++ stats/01_summary_stats/summary_stats.py
@@ -1,5 +1,15 @@
 def mean(lst, trim_by=0):
-    pass
+    lst_ = lst.copy()
+
+    if trim_by > 0:
+        lst_ = sorted(lst_)[trim_by:-trim_by]
+
+    return sum(lst_) / len(lst_)
+
 
 # data from the prior slide
 a = [1, 5, 7, 10, 15, 23, 35, 67, 220, 2000]
+
+print(a)
+print(mean(a))
+print(mean(a, trim_by=1))
