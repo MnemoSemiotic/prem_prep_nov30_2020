894eedca63bf76d696e342fde09b8c9bcce64ad9
Tovio Roberts <clownfragment@gmail.com>
2020-12-08 20:58:53 -0700

    Auto Updated: Tue 58:20 Dec 08 2020

--- stats/01_summary_stats/summary_stats.py
+++ stats/01_summary_stats/summary_stats.py
@@ -148,7 +148,15 @@ for _ in range(5):
 Mode
 '''
 def mode(lst):
-    pass
+    most_occurring = lst[0]
+
+    for item in lst[1:]:
+        if lst.count(item) > lst.count(most_occurring):
+            most_occurring = item
+
+    return most_occurring
 
 
 mode_lst = ['kangaroo', 'muskrat', 'platypus', 'muskrat', 'squid', 'squirrel', 'muskrat']
+
+print(most_occurring(mode_lst))
