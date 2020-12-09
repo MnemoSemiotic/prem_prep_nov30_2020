f4bfeed489d09dd0411de954d197cd2ce456ad79
Tovio Roberts <clownfragment@gmail.com>
2020-12-08 19:51:55 -0700

    Auto Updated: Tue 51:19 Dec 08 2020

--- stats/01_summary_stats/summary_stats.py
+++ stats/01_summary_stats/summary_stats.py
@@ -29,11 +29,13 @@ A. Determine the sample mean for each group.
 urban = [6.0, 5.0, 11.0, 33.0, 4.0, 5.0, 80.0, 18.0, 35.0, 17.0, 23.0]
 farmhouse = [4.0, 14.0, 11.0, 9.0, 9.0, 8.0, 4.0, 20.0, 5.0, 8.9, 21.0, 9.2, 3.0, 2.0, 0.3]
 
-print(f'Sample Mean Urban: {round(mean(urban), 1)}')
-print(f'Sample Mean Farmhouse: {round(mean(farmhouse), 1)}')
+# print(f'Sample Mean Urban: {round(mean(urban), 1)}')
+# print(f'Sample Mean Farmhouse: {round(mean(farmhouse), 1)}')
 
 '''
 B. Determine the trimmed mean for each group by trimming the smallest and largest value from each group.
 '''
-print(f'Sample Trimmed Mean Urban: {round(mean(urban, trim_by=1), 1)}')
-print(f'Sample Trimmed Mean Farmhouse: {round(mean(farmhouse, trim_by=1), 1)}')
+# print(f'Sample Trimmed Mean Urban: {round(mean(urban, trim_by=1), 1)}')
+# print(f'Sample Trimmed Mean Farmhouse: {round(mean(farmhouse, trim_by=1), 1)}')
+
+
