5c9888b48c09c32053f48377a89b43fedf10dd06
Tovio Roberts <clownfragment@gmail.com>
2020-12-08 20:08:27 -0700

    Auto Updated: Tue 08:20 Dec 08 2020

--- stats/01_summary_stats/summary_stats.py
+++ stats/01_summary_stats/summary_stats.py
@@ -50,7 +50,9 @@ def median(lst):
     if len(lst) % 2 == 1:
         mid = int(len(lst) / 2)
         return lst_sorted[mid]
-
+    else:
+        upper_mid_idx = int(len(lst)/2)
+        return mean([lst_sorted[upper_idx-1], lst_sorted[upper_idx]])
 
 odd = [13, 18, 13, 14, 13, 16, 35, 21, 13]
 even = [15, 14, 10, 8, 12, 8, 16, 13]
