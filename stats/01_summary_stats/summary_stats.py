ce16608c432221bcdc812e05f78ec53f53f6abcd
Tovio Roberts <clownfragment@gmail.com>
2020-12-12 13:09:50 -0700

    Auto Updated: Sat 09:13 Dec 12 2020

--- stats/01_summary_stats/summary_stats.py
+++ stats/01_summary_stats/summary_stats.py
@@ -252,16 +252,18 @@ Breakout slide 30
 Consider the dataset from the previous question:
 '''
 a =  [590, 615, 575, 608, 350, 1285, 408, 540, 555, 679]
-print(sorted(a))
+# print(sorted(a))
 
 '''Calculate the five number summary for the data
 (mean) = 620.5, median = 582.5'''
 
 '''Determine the IQR of the Dataset'''
-print('iqr', iqr(a))
+# print('iqr', iqr(a))
 
 '''Determine whether any of the data points can be defined as outliers. If so, what are the outliers?
 '''
-print('outliers: ',detect_outliers(a))
+# print('outliers: ',detect_outliers(a))
 
 '''What is the best measure of centrality for this data?'''
+
+
