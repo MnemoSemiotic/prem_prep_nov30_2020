cb62cfb3690a47bf74ce50943f5793846b1806ca
Tovio Roberts <clownfragment@gmail.com>
2020-12-15 19:20:57 -0700

    Auto Updated: Tue 20:19 Dec 15 2020

--- stats/02_sets/sets.py
+++ stats/02_sets/sets.py
@@ -8,5 +8,16 @@ def coin_flip():
     # return choice('HT') # equivalent alternative
     return choice(['H', 'T'])
 
-print(coin_flip())
-    
+# print(coin_flip())
+
+
+# 20 flips
+def twenty_flips():
+    flips = []
+
+    for _ in range(20):
+        flips.append(coin_flip())
+
+    return flips
+
+print(twenty_flips())
