2805a37efdca2fdc161fc0480849d6052e03fac6
Tovio Roberts <clownfragment@gmail.com>
2020-12-15 20:30:00 -0700

    Auto Updated: Tue 30:20 Dec 15 2020

--- stats/02_sets/sets.py
+++ stats/02_sets/sets.py
@@ -152,5 +152,7 @@ def difference(set1, set2):
     return set_difference
 
 
-print(difference(list1, list2))
-print(difference(list2, list1))
+# print(difference(list1, list2))
+# print(difference(list2, list1))
+
+
