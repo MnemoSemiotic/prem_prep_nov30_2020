826fbd63240d7451a45f297d50ae01ac4c6ed402
Tovio Roberts <clownfragment@gmail.com>
2020-12-15 19:52:22 -0700

    Auto Updated: Tue 52:19 Dec 15 2020

--- stats/02_sets/sets.py
+++ stats/02_sets/sets.py
@@ -67,7 +67,7 @@ def star_args(*args):
         print(item)
     return None
 
-star_args('hi', 0.546, 'elephant', 5, True, None, [1,2,3,4,6])
+# star_args('hi', 0.546, 'elephant', 5, True, None, [1,2,3,4,6])
 
 
 
@@ -90,3 +90,15 @@ def union(set1, set2):
 
 # print(union(list1, list2))
 
+
+def union_mult_sets(*args):
+    set_union = []
+
+    for lst in args:
+        for item in lst:
+            if item not in set_union:
+                set_union.append(item)
+    
+    return set_union
+
+print(list1, list2, list3)
