d2970bd44e1a90cfb0cd7558ae1879af62f9e7d4
Tovio Roberts <clownfragment@gmail.com>
2020-12-17 20:21:40 -0700

    Auto Updated: Thu 21:20 Dec 17 2020

--- stats/03_basic_probability/basic_proba.py
+++ stats/03_basic_probability/basic_proba.py
@@ -10,3 +10,10 @@ Write a function called series_of_flips.
   define one parameter, n, which represents the number of coin flips
   return a length n list of the random coin flips
 '''
+def series_of_flips(n):
+    flips = []
+    for _ in range(n):
+        flips.append(coin_flip())
+    return flips
+
+print(series_of_flips(4))
