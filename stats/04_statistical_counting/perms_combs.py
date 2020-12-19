56690a8460b786f112086f96e74f27ae8b644d82
Tovio Roberts <clownfragment@gmail.com>
2020-12-19 12:58:40 -0700

    Auto Updated: Sat 58:12 Dec 19 2020

---
+++ stats/04_statistical_counting/perms_combs.py
@@ -0,0 +1,18 @@
+five_toys = ['red truck', 'pog', 'fidget spinner', 'tamagotchi', 'gi joe']
+
+'''
+You have 5 toys in a bag. You pull them out one at a time and 
+you don't put them back in. In how many different orders
+can you pull out these 5 toys?
+
+5! = 120
+
+['red truck', 'pog', 'fidget spinner', 'tamagotchi', 'gi joe']
+['red truck', 'pog', 'fidget spinner', 'gi joe', 'tamagotchi']
+['red truck', 'pog', 'gi joe', 'fidget spinner', 'tamagotchi']
+...
+
+'''
+
+def factorial(n):
+    pass
