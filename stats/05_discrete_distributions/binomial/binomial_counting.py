ff9d0259a5c4442c874eff8d9f0d50d12d82e697
Tovio Roberts <clownfragment@gmail.com>
2020-12-21 20:10:11 -0700

    Auto Updated: Mon 10:20 Dec 21 2020

--- stats/05_discrete_distributions/binomial/binomial_counting.py
+++ stats/05_discrete_distributions/binomial/binomial_counting.py
@@ -69,5 +69,25 @@ def gen_4_bit_binary():
                     decimal += 1
     return bin_dct
 
-for dec, bin_ in gen_4_bit_binary().items():
-    print(f'{dec}: {bin_}')
+# for dec, bin_ in gen_4_bit_binary().items():
+#     print(f'{dec}: {bin_}')
+
+
+'''
+7 minutes!
+
+Code the dec_to_bin function.
+Should take as input 2 things:
+    a decimal value (dec)
+    a number of bits (n_bits=8)
+
+algorithm
+Given a decimal number
+    - take the modulus by 2
+        - set aside the result
+    - floor divide the number by 2
+        - until that number is 0
+    - reverse the sequence of outcomes
+
+dec_to_bin(43)
+'''
