3b48e4271bd96e92a854dd9160c4293a1ff902e5
Tovio Roberts <clownfragment@gmail.com>
2020-12-21 20:58:10 -0700

    Auto Updated: Mon 58:20 Dec 21 2020

---
+++ stats/05_discrete_distributions/binomial/binomial_sampling.py
@@ -0,0 +1,18 @@
+'''
+Generalized Analytic Approach
+1. Observe/Synthesize some phenomenon in nature
+    - ex. sitting on a park bench and looking at the feet of
+          geese, and marking 1 if they have black feet or 0
+          if they have orange feet.
+    - ex. counting in binary and summing the bits (for a fixed length of bits)
+
+2. Create a means to organize and observe our results
+    - ex. **packing values into a dictionary and printing them to terminal
+
+3. Describe or interpret the phenomenon through the lens of our observations
+
+'''
+
+'''
+
+'''
