e5c15ff924b5b8adb9488bb78c0bb5f6dd5c2775
Tovio Roberts <clownfragment@gmail.com>
2020-12-22 19:35:40 -0700

    Auto Updated: Tue 35:19 Dec 22 2020

--- stats/05_discrete_distributions/binomial/binomial_using_pmf.py
+++ stats/05_discrete_distributions/binomial/binomial_using_pmf.py
@@ -4,4 +4,27 @@ def factorial(n):
         prod *= num
     return prod
 
-def com
+
+def combinations(n, k):
+    return int(factorial(n) / (factorial(n-k) * factorial(k)))
+
+def combinations(n, k):
+    perm = 1
+    for i in range(n, n-k, -1):
+        perm *= 1
+    return int(perm / factorial(k))
+
+
+'''
+PMF: Probability Mass Function
+- Giving us the probability of a certain specific outcome
+- can answer the binomial questions
+- 3 params: 
+    n : number of trials
+    k : represents the number of successes
+    p : is the probability of success of single trial (held constant)
+
+What is the probability in 12 coin flips of a fair coin, that you get 7 heads?
+'''
+
+def binomial_pmf(n, k, p=0.5):
