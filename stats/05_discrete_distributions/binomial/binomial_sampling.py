d28db54081c39f4d11298d0408ab20f5031f9515
Tovio Roberts <clownfragment@gmail.com>
2020-12-22 20:31:51 -0700

    Auto Updated: Tue 31:20 Dec 22 2020

--- stats/05_discrete_distributions/binomial/binomial_sampling.py
+++ stats/05_discrete_distributions/binomial/binomial_sampling.py
@@ -13,6 +13,20 @@ Generalized Analytic Approach
 
 '''
 
-'''
+from random import choice, random
 
 '''
+using choice we are limited to a fixed value of p
+'''
+
+def get_bit():
+    return choice([0,1])
+
+def generate_n_bits(n=8):
+    return [get_bit() for _ in range(n)]
+    # lst = []
+    # for _ in range(n):
+    #     lst.append(get_bit())
+    # return lst
+
+print(generate_n_bits(n=16))
