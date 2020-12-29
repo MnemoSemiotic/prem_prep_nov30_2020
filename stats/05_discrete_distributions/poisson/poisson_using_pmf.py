084ba598340c76322076854dc00f87e46bc4387b
Tovio Roberts <clownfragment@gmail.com>
2020-12-28 19:53:01 -0700

    Auto Updated: Mon 53:19 Dec 28 2020

--- stats/05_discrete_distributions/poisson/poisson_using_pmf.py
+++ stats/05_discrete_distributions/poisson/poisson_using_pmf.py
@@ -9,11 +9,16 @@ from math import e
 
 # print(e) # 2.718281828459045
 
-def factorial():
-    pass
+def factorial(n):
+    prod = 1
+
+    for num in range(1, n+1):
+        prod *= num
+    return prod
 
 def poisson_pmf(lmbda, k):
     '''
     lmbda represents the average number of successes in a given volume of time or space, etc
     k is the number of successes for which you're trying to find the probability
     '''
+    
