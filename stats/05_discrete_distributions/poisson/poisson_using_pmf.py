3a0ad3c2ec0a7a18023fcdb5dae64eb346e356e0
Tovio Roberts <clownfragment@gmail.com>
2020-12-28 19:29:07 -0700

    Auto Updated: Mon 29:19 Dec 28 2020

---
+++ stats/05_discrete_distributions/poisson/poisson_using_pmf.py
@@ -0,0 +1,6 @@
+'''
+You have a satellite that takes stellar images of random places in space. 
+On average, each image has 30 stars in it. 
+What is the probability that a given image has 25 stars in it?
+
+'''
