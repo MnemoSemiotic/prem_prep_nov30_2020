1c4f60206857c7f9a28a2b96a0e7588dd9a238b9
Tovio Roberts <clownfragment@gmail.com>
2020-12-28 17:12:09 -0700

    Auto Updated: Mon 12:17 Dec 28 2020

--- stats/04_statistical_counting/perms_combs_redo.py
+++ stats/04_statistical_counting/perms_combs_redo.py
@@ -9,3 +9,19 @@ def factorial(num):
     return prod
 
 # print(factorial(5))
+
+'''
+Permutations
+nPk = n! / (n-k)!
+if k==n =>
+    5! / (5-5)!
+    5! / 0!
+    120 / 1
+'''
+
+
+'''
+You have 10 students and you are conducting a science fair where 4 students will win
+1st, 2nd, 3rd, 4th. How many different arrangements of those 4 winners is possible?
+
+'''
