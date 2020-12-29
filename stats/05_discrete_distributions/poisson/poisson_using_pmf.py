3887171483b9a771bcfc44edd21d74b54ad593ef
Tovio Roberts <clownfragment@gmail.com>
2020-12-28 19:49:52 -0700

    Auto Updated: Mon 49:19 Dec 28 2020

--- stats/05_discrete_distributions/poisson/poisson_using_pmf.py
+++ stats/05_discrete_distributions/poisson/poisson_using_pmf.py
@@ -7,4 +7,13 @@ What is the probability that a given image has 25 stars in it?
 
 from math import e
 
-print(e)
+# print(e) # 2.718281828459045
+
+def factorial():
+    pass
+
+def poisson_pmf(lmbda, k):
+    '''
+    lmbda represents the average number of successes in a given volume of time or space, etc
+    k is the number of successes for which you're trying to find the probability
+    '''
