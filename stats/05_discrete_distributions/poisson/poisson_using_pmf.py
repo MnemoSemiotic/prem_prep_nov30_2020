91c5c1553863bb1d222c8b6a111b52268be80607
Tovio Roberts <clownfragment@gmail.com>
2020-12-28 19:56:13 -0700

    Auto Updated: Mon 56:19 Dec 28 2020

--- stats/05_discrete_distributions/poisson/poisson_using_pmf.py
+++ stats/05_discrete_distributions/poisson/poisson_using_pmf.py
@@ -17,3 +17,14 @@ def poisson_pmf(lmbda, k):
     return lmbda**k * e**(-lmbda) / factorial(k)
 
 # print(poisson_pmf(lmbda=10, k=10))
+
+'''
+You have a satellite that takes stellar images of random places in space. 
+On average, each image has 30 stars in it. 
+What is the probability that a given image has 25 stars in it?
+'''
+
+lmbda = 30
+k = 25
+
+print(poisson_pmf(lmbda, k))
