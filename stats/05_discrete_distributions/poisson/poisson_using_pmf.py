3ae49dff512d7e904be48fe5ce1cb4c0ea4a22e0
Tovio Roberts <clownfragment@gmail.com>
2020-12-28 19:55:08 -0700

    Auto Updated: Mon 55:19 Dec 28 2020

--- stats/05_discrete_distributions/poisson/poisson_using_pmf.py
+++ stats/05_discrete_distributions/poisson/poisson_using_pmf.py
@@ -1,10 +1,3 @@
-'''
-You have a satellite that takes stellar images of random places in space. 
-On average, each image has 30 stars in it. 
-What is the probability that a given image has 25 stars in it?
-
-'''
-
 from math import e
 
 # print(e) # 2.718281828459045
@@ -21,4 +14,6 @@ def poisson_pmf(lmbda, k):
     lmbda represents the average number of successes in a given volume of time or space, etc
     k is the number of successes for which you're trying to find the probability
     '''
-    
+    return lmbda**k * e**(-lmbda) / factorial(k)
+
+# print(poisson_pmf(lmbda=10, k=10))
