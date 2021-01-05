d501187205b617fa078fe8042d22c873aae67c99
Tovio Roberts <clownfragment@gmail.com>
2021-01-04 21:00:00 -0700

    Auto Updated: Mon 00:21 Jan 04 2021

--- stats/05_discrete_distributions/geometric/geometric_problems.py
+++ stats/05_discrete_distributions/geometric/geometric_problems.py
@@ -26,3 +26,13 @@ Differentiating Binomial, Poisson, Geometric, and basic probability
 # Geometric
 # The proba of some binary event is p. What is the probability of having your
 # first success on the kth trial?
+
+
+# Poisson
+# On average, some number of events happens in this volume (of time, space, etc).
+
+# What is the probability of some (other) number of events happen in this volume
+# or in some expansion or contraction of this volume?
+
+
+# Basic Probability
