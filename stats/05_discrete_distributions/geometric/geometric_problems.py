42ffa37a11c8b8f51cbe439ca9ef1b8f992135e3
Tovio Roberts <clownfragment@gmail.com>
2021-01-04 20:53:35 -0700

    Auto Updated: Mon 53:20 Jan 04 2021

--- stats/05_discrete_distributions/geometric/geometric_problems.py
+++ stats/05_discrete_distributions/geometric/geometric_problems.py
@@ -9,3 +9,10 @@ What is the probability that the first head will be on the 3rd trial?
 p = 0.32
 k = 3
 print(geometric_pmf(p, k, inclusive=True))
+
+
+
+'''
+Differentiating Binomial, Poisson, Geometric, and basic probability
+'''
+
