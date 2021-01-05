b55083d477b97d6769ced8cf6cb1fdd72fb789c6
Tovio Roberts <clownfragment@gmail.com>
2021-01-04 20:27:18 -0700

    Auto Updated: Mon 27:20 Jan 04 2021

--- stats/05_discrete_distributions/geometric/geometric_sampling.py
+++ stats/05_discrete_distributions/geometric/geometric_sampling.py
@@ -5,3 +5,18 @@ def bernoulli(p=0.5):
         return 1
     else:
         return 0
+
+
+def perform_geometric(p=0.5):
+    num_trials = 0
+    for _ in range(2000000):
+        flip = bernoulli(p)
+        num_trials += 1
+
+        print(f'trial: {flip}')
+        if flip == 1:
+            break
+
+    print(f'Success on the {num_trials} trial')
+
+perform_geometric(p=0.5)
