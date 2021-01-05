50f7d37e96c3e733111139c4294c35dfd2c9702a
Tovio Roberts <clownfragment@gmail.com>
2021-01-04 20:52:29 -0700

    Auto Updated: Mon 52:20 Jan 04 2021

--- stats/05_discrete_distributions/geometric/geometric_problems.py
+++ stats/05_discrete_distributions/geometric/geometric_problems.py
@@ -6,4 +6,6 @@ Geometric Breakout 1
 Suppose you have an unfair coin, with a 68% chance of getting tails. 
 What is the probability that the first head will be on the 3rd trial?
 '''
-p = 
+p = 0.32
+k = 3
+print(geometric_pmf(p, k, inclusive=True))
