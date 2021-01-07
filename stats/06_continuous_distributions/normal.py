b89f437f4d9ad988341aa102c7886c5c81a6f1fd
Tovio Roberts <clownfragment@gmail.com>
2021-01-06 20:41:57 -0700

    Auto Updated: Wed 41:20 Jan 06 2021

--- stats/06_continuous_distributions/normal.py
+++ stats/06_continuous_distributions/normal.py
@@ -3,3 +3,12 @@ from math import e, pi, sqrt
 def normal_pdf(x=0, mu=0, sigma=1):
     return (1 / (sigma * sqrt(2 * pi))) * (e**(-(1/2))) * (((x - mu) / sigma)**2)
 
+
+def normal_cdf(x=0, mu=0, sigma=1):
+    vals = [num*0.001 for num in range(-1000, int(x*1000))]
+
+    for val in vals:
+        print(val)
+
+
+print(normal_cdf())
