2d1ef950196a44d0853edddd28bd55825208e862
Tovio Roberts <clownfragment@gmail.com>
2021-01-06 19:35:43 -0700

    Auto Updated: Wed 35:19 Jan 06 2021

--- stats/06_continuous_distributions/uniform.py
+++ stats/06_continuous_distributions/uniform.py
@@ -5,4 +5,13 @@ def get_bit():
 
 
 def get_binary(n=8):
-    pass
+    return [get_bit() for _ in range(n)]
+
+    # return_list = []
+
+    # for _ in range(n):
+    #     return_list.append(get_bit())
+
+    # return return_list
+
+print(get_binary(16))
