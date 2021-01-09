9a8d6477b64eb233846defd81cc468360064ee72
Tovio Roberts <clownfragment@gmail.com>
2021-01-09 13:22:14 -0700

    Auto Updated: Sat 22:13 Jan 09 2021

--- stats/08_preparing_for_ti/ti_prep.py
+++ stats/08_preparing_for_ti/ti_prep.py
@@ -29,10 +29,13 @@ TI Skills/Outline
 '''
 
 def factorial(n):
-    pass
+    prod = 1
+    for  num in range(1, n+1):
+        prod *= num
+    return prod
 
 def combinations(n, k):
-    pass
+    return factorial(n) / (factorial(n-k)*factorial(k))
 
 def binomial_pmf(n, k, p=0.5):
-    pass
+    return combinations(n, k) * (p**k) * ((1-p)**(n-k))
